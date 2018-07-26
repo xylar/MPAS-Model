387c0f470330641279b6c6ab4751b5fab031b1e6
Steven Brus <sbrus@lanl.gov>
2018-07-26 17:40:01 -0600

    Changes to coastal_mesh_definition_tools.py
     - Use US Atlantic Coast as a test case
    
     - Improve transition function
    
     - Increase flann precision
    
     - Improve extracted coastline plotting

--- testing_and_setup/compass/ocean/jigsaw_to_MPAS/coastal_mesh_definition_tools.py
+++ testing_and_setup/compass/ocean/jigsaw_to_MPAS/coastal_mesh_definition_tools.py
@@ -25,7 +25,8 @@ nn_search = "flann"
 # Bounding boxes
 Delaware_Bay =  np.array([-75.61903,-74.22, 37.767484, 40.312747])
 Galveston_Bay = np.array([-95.45,-94.4, 29, 30])
-US_Atlantic_Coast = np.array([-98.186645, -59.832744, 7.791301 ,45.942453])
+US_Atlantic_Coast = np.array([-81.7,-62.3,25.1,46.24])
+US_Atlantic_Gulf_Coast = np.array([-98.186645, -59.832744, 7.791301 ,45.942453])
 Delware_Region = np.array([-77, -69.8 ,35.5, 41])
 Entire_Globe = np.array([-180,180,-90,90])
 
@@ -37,7 +38,7 @@ data_path = "/users/sbrus/climate/bathy_data/SRTM15_plus/"
 nc_file = "earth_relief_15s.nc"
 
 # Bounding box of coastal refinement region
-region_box = Delaware_Bay
+region_box = US_Atlantic_Coast
 origin = (.5*(region_box[0]+region_box[1]), .5*(region_box[2]+region_box[3]))
 
 # Mesh parameters
@@ -45,10 +46,11 @@ grd_box = Entire_Globe
 ddeg = .1
 dx_min = 1*km
 dx_max = 240*km
-grade_width = 10000*km
+trans_width = 10000*km
+trans_start = 5000*km
 
 # Bounding box of plotting region
-plot_box = US_Atlantic_Coast 
+plot_box = US_Atlantic_Gulf_Coast 
 
 ######################################################################################
 ######################################################################################
@@ -73,46 +75,43 @@ print z_region.shape
 m = Basemap(projection='cyl',llcrnrlat=plot_box[2],urcrnrlat=plot_box[3],\
             llcrnrlon=plot_box[0],urcrnrlon=plot_box[1],resolution='c')
 
-# Plot the bathymetry data
-plt.figure()
-levels = np.linspace(np.amin(z_region),np.amax(z_region),100)
-plt.contourf(lon_region,lat_region,z_region,levels=levels)
-plt.colorbar()
-plt.axis('equal')
-plt.savefig('bathy.png',bbox_inches='tight')
 
 # Find coastline contours, filter out small strings
+print "Extracting coastline"
 contours = measure.find_contours(z_region,0)
 contours.sort(key=len,reverse=True)
 coastlines = []
 for c in contours[:10]:
   if c.shape[0] > 20:
-    coastlines.append(c)
-
-# Plot coastlines
-plt.figure()
-for c in coastlines:
-  plt.plot(c[:,1],c[:,0])
-plt.axis('equal')
-plt.savefig('coastlines.png',bbox_inches='tight')
+    cpad= np.vstack((c,[np.nan,np.nan]))
+    coastlines.append(cpad)
+print "Done"
 
 # Combine coastlines and map from pixel to lon,lat
 coast = np.concatenate(coastlines)
 coast_pts = np.copy(coast)
 coast_pts[:,0] = (region_box[1]-region_box[0])/float(len(lon_region))*coast[:,1] + region_box[0]
 coast_pts[:,1] = (region_box[3]-region_box[2])/float(len(lat_region))*coast[:,0] + region_box[2]
+coast_pts_plt = np.copy(coast_pts)
+
+# Plot bathymetry data and coastlines
 plt.figure()
-plt.plot(coast_pts[:,0],coast_pts[:,1])
-plt.savefig('coastlines_latlon.png',bbox_inches='tight')
+levels = np.linspace(np.amin(z_region),np.amax(z_region),100)
+plt.contourf(lon_region,lat_region,z_region,levels=levels)
+plt.plot(coast_pts_plt[:,0],coast_pts_plt[:,1],color='white')
+plt.colorbar()
+plt.axis('equal')
+plt.savefig('bathy_coastlines.png',bbox_inches='tight')
 
 # Convert to x,y and create kd-tree
+coast_pts = coast_pts[np.isfinite(coast_pts).all(axis=1)]
 coast_pts_xy = np.copy(coast_pts)
 coast_pts_xy[:,0],coast_pts_xy[:,1] = CPP_projection(coast_pts[:,0],coast_pts[:,1],origin)
 if nn_search == "kdtree":
   tree = spatial.KDTree(coast_pts_xy)
 elif nn_search == "flann":
   flann = pyflann.FLANN()
-  flann.build_index(coast_pts_xy,algorithm='kdtree',target_precision=.9999)
+  flann.build_index(coast_pts_xy,algorithm='kdtree',target_precision=.99999)
 
 # Create cell width background grid
 lat_grd = np.arange(grd_box[2],grd_box[3],ddeg)
@@ -131,7 +130,7 @@ start = timeit.default_timer()
 if nn_search == "kdtree":
   d,idx = tree.query(pts)
 elif nn_search == "flann":
-  idx,d = flann.nn_index(pts,checks=150)
+  idx,d = flann.nn_index(pts,checks=300)
   d = np.sqrt(d)
 end = timeit.default_timer()
 print "Done"
@@ -153,6 +152,8 @@ print D_plot.shape,D.shape
 plt.figure()
 levels = np.linspace(np.amin(D_plot),np.amax(D_plot),100)
 plt.contourf(lon_grd_plot,lat_grd_plot,D_plot,levels=levels)
+m.drawcoastlines()
+plt.plot(coast_pts_plt[:,0],coast_pts_plt[:,1],color='white')
 #plt.plot(Lon_grd,Lat_grd,'k-',lw=0.5,alpha=0.5)
 #plt.plot(Lon_grd.T,Lat_grd.T,'k-',lw=0.5,alpha=0.5)
 plt.colorbar()
@@ -162,9 +163,12 @@ plt.savefig('distance.png',bbox_inches='tight')
 # Assign background grid cell width values
 cell_width = dx_max*np.ones(D.shape)
 
-# Compute cell width based on distance
-cell_width_dist = dx_max*np.tanh(1.0/(2*grade_width)*D)+dx_min
-cell_width = np.minimum(cell_width_dist,cell_width)/km
+# Compute cell width based on distanceA
+backgnd_weight = .5*(np.tanh((D-trans_start)/trans_width)+1)
+dist_weight = 1-backgnd_weight
+cell_width = (dx_min*dist_weight+dx_max*backgnd_weight)/km
+#cell_width_dist = dx_max*np.tanh(1.0/(2*trans_width)*D)+dx_min
+#cell_width = np.minimum(cell_width_dist,cell_width)/km
 cell_width_plot = cell_width[np.ix_(lat_idx,lon_idx)]
 
 # Save matfile
@@ -173,6 +177,7 @@ io.savemat('cellWidthVsLatLon.mat',mdict={'cellWidth':cell_width,'lon':lon_grd,'
 plt.figure()
 plt.contourf(lon_grd_plot,lat_grd_plot,cell_width_plot)
 m.drawcoastlines()
+plt.plot(coast_pts_plt[:,0],coast_pts_plt[:,1],color='white')
 plt.colorbar()
 plt.axis('equal')
 plt.savefig('cell_width.png',bbox_inches='tight')
